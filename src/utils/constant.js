8ad77ce591854d8f3a93f027294ec7aee6ab4f77
San Prastiwa <san.prastiwa@gmail.com>
2023-06-02 14:42:25 +0700

    create context and reducer

---
+++ src/utils/constant.js
@@ -0,0 +1,2 @@
+export const KELAS_TARI = "kelas tari";
+export const KELAS_LUKIS = "kelas lukis";
---
+++ src/utils/images.js
@@ -0,0 +1,23 @@
+import logo_navbar from "../assets/logo_nav.png";
+
+import kelastari1 from "../assets/kelastari1.png";
+import kelastari2 from "../assets/kelastari2.jpg";
+import kelaslukis1 from "../assets/kelastari1.png";
+import kelaslukis2 from "../assets/kelaslukis2.png";
+import kelaslukis3 from "../assets/kelaslukis3.png";
+import kelaslukis4 from "../assets/kelaslukis4.png";
+
+const course_images = {
+  kelastari1,
+  kelastari2,
+  kelaslukis1,
+  kelaslukis2,
+  kelaslukis3,
+  kelaslukis4,
+};
+
+const other_images = {
+  logo_navbar,
+};
+
+export { kelas_images, other_images };
