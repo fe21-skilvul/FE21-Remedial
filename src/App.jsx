c926ef97b81dd359d2bf449d70a44f9d92884a54
Chitra Agustina <129734442+Citraagustin21@users.noreply.github.com>
2023-06-05 13:45:24 +0700

    Add files via upload
    upload file app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import Landing from "./components/Landing";
 import Ratings from "./components/Ratings";
 import Class from "./components/Class";
 import Remidial from "./components/Remidial";
+import Header from "./components/Header";
+import Cover from "./components/Cover";
+import Program from "./components/Program";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Landing/>
-      <Ratings/>
-      <Class/>
-      <Remidial/>
+      <Header />
+      <Cover />
+      <Landing />
+      <Class />
+      <Ratings />
+      <Remidial />
+      <Program />
+      <Footer />
     </>
   );
 }
